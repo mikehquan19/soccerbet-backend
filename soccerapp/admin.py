57fbdc29d560320bdb6d6993e3ed2b3bcb364623
mikehquan19 <hoangphucquan19@gmail.com>
2024-12-22 04:40:57 -0600

    Add modifed models to admin site

--- soccerapp/admin.py
+++ soccerapp/admin.py
@@ -4,11 +4,11 @@ from . import models
 # Register your models here.
 admin.site.register(models.User)
 admin.site.register(models.Team)
+admin.site.register(models.TeamRanking)
 admin.site.register(models.Match)
 admin.site.register(models.MoneylineBetInfo)
 admin.site.register(models.UserMoneylineBet)
 admin.site.register(models.HandicapBetInfo)
 admin.site.register(models.UserHandicapBet)
-admin.site.register(models.TotalGoalsBetInfo)
-admin.site.register(models.UserTotalGoalsBet)
-
+admin.site.register(models.TotalObjectsBetInfo)
+admin.site.register(models.UserTotalObjectsBet)
