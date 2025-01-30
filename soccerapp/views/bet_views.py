9ee4bc3cadd3278ff6aeefd606d69c56d7f84250
mikehquan19 <hoangphucquan19@gmail.com>
2025-01-30 02:22:42 -0600

    Fix the existing views to use query params and add new views for comments

--- soccerapp/views/bet_views.py
+++ soccerapp/views/bet_views.py
@@ -3,62 +3,71 @@ VIEWS FOR USER'S BET LIST
 NOTE: THE VIEWS HERE WILL BE ONLY ACCESSIBLE BY AUTHENTICATED USER,
     SO THEY WILL BE IMPLEMENTED PERMISSION CLASSES ISAUTHENTICATED 
 """
-
 from django.shortcuts import get_object_or_404
 from django.db import transaction
 from rest_framework import status, generics
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from soccerapp.models import User, UserMoneylineBet, UserHandicapBet, UserTotalObjectsBet
+from soccerapp.models import UserMoneylineBet, UserHandicapBet, UserTotalObjectsBet
 from soccerapp.serializers import (
-    UserSerializer,
+    UserSerializer, 
     UserMoneylineBetSerializer, UserHandicapBetSerializer, UserTotalObjectsBetSerializer
 )
+from soccerapp.serializers import CustomValidator
+
+# the validator used to validate the DELETE method endpoint 
+bet_validator = CustomValidator() 
 
 # views to show detail of the user 
 class UserDetail(generics.RetrieveAPIView): 
-    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user # get the user from the request
 
 
 # views to list all of the moneyline bets and create new bet
 class UserMoneylineBetList(APIView): 
-    # get the response data (list of moneyline bet of the given user)
-    def get_response_data(self, user_id) -> list: 
-        queried_user = get_object_or_404(User, id=user_id)
-        moneyline_bet_list = UserMoneylineBet.objects.filter(user=queried_user)
-        bet_list_serializer = UserMoneylineBetSerializer(moneyline_bet_list, many=True)
-        return bet_list_serializer.data
+    permission_classes = [IsAuthenticated]
 
     # the GET method 
-    def get(self, request, user_id, format=None) -> Response: 
-        moneyline_bet_data = self.get_response_data(user_id)
-        return Response(moneyline_bet_data)
+    def get(self, request, format=None) -> Response: 
+        status = request.query_params.get("status")
+        # get the response data and return 
+        if status is not None: 
+            moneyline_bet_list = UserMoneylineBet.objects.filter(user=request.user, bet_info__status=status)
+        else: 
+            moneyline_bet_list = UserMoneylineBet.objects.filter(user=request.user)
+        bet_list_serializer = UserMoneylineBetSerializer(moneyline_bet_list, many=True)
+        return Response(bet_list_serializer.data)
     
     # the POST method 
-    def post(self, request, user_id, format=None) -> Response: 
+    def post(self, request, format=None) -> Response: 
         request_data = request.data
-        for item_data in request_data: item_data["user"] = user_id
+        for item_data in request_data: item_data['user'] = request.user.pk # add the user field to request data
 
-        new_bet_serializer = UserMoneylineBetSerializer(data=request_data, many=True)
-        if new_bet_serializer.is_valid(): 
+        new_bet_list_serializer = UserMoneylineBetSerializer(data=request_data, many=True)
+        if new_bet_list_serializer.is_valid(): 
             with transaction.atomic(): # maintain the integrity of the data
                 # save the new bet to the database 
-                _, total_bet_amount = new_bet_serializer.save() 
-                # adjust the balance of the user after saving 
-                queried_user = get_object_or_404(User, id=user_id)
-                queried_user.balance -= total_bet_amount
-                queried_user.save()
+                new_moneyline_bet, total_bet_amount = new_bet_list_serializer.save()
 
-            # return the new list of bets (in which the new bets have been added)
-            new_moneyline_bet_data = self.get_response_data(user_id)
-            return Response(new_moneyline_bet_data, status=status.HTTP_201_CREATED)
+                # adjust the balance of the user after saving 
+                bet_owner = request.user
+                bet_owner.balance -= total_bet_amount
+                bet_owner.save()
+            # return the reserialized to ensure the returned data is complete
+            return Response(UserMoneylineBetSerializer(new_moneyline_bet, many=True).data, status=status.HTTP_201_CREATED)
         # return errors if the data is invalid 
-        return Response(new_bet_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(new_bet_list_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # views to handle the detail of the moneyline bets with given pk 
 class UserMoneylineBetDetail(APIView): 
+    permission_classes = [AllowAny]
+
     # the GET method 
     def get(self, request, pk, format=None) -> Response: 
         queried_moneyline_bet = get_object_or_404(UserMoneylineBet, pk=pk)
@@ -76,11 +85,10 @@ class UserMoneylineBetDetail(APIView):
                 updated_bet = updated_bet_serializer.save()
 
                 # adjust the balance of the user 
-                queried_user = updated_bet.user 
+                bet_owner = updated_bet.user 
                 bet_amount_difference = old_bet_amount - updated_bet.bet_amount
-                queried_user.balance += bet_amount_difference
-                queried_user.save()
-
+                bet_owner.balance += (bet_amount_difference * 1.05) # include the extra fees for placing the bets
+                bet_owner.save()
             # return the updated data
             return Response(updated_bet_serializer.data, status=status.HTTP_202_ACCEPTED)
         # return the errors 
@@ -89,19 +97,21 @@ class UserMoneylineBetDetail(APIView):
     # the DELETE method 
     def delete(self, request, pk, format=None) -> Response: 
         queried_moneyline_bet = get_object_or_404(UserMoneylineBet, pk=pk)
+        bet_validator.validate_delete(queried_moneyline_bet) # validate if the instance (bet) is elligible for withdrawing
+
         with transaction.atomic(): 
             # return the amount the user bet back to the user
-            this_user = queried_moneyline_bet.user 
-            this_user.balance += queried_moneyline_bet.bet_amount 
-            this_user.save()
-
+            bet_owner = queried_moneyline_bet.user 
+            bet_owner.balance += queried_moneyline_bet.bet_amount 
+            bet_owner.save()
+            
             # delete the monyeline bet from the database 
             queried_moneyline_bet.delete()
         return Response({"message": "Bet deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
 
 
 """ 
-generics views will be used for the remaining 2 types of bet for easier life 
+Generics views will be used for the remaining 2 types of bet for easier life 
 """ 
 # base views for listing and creating of all types of view
 class UserBetList(generics.ListCreateAPIView): 
@@ -113,52 +123,57 @@ class UserBetList(generics.ListCreateAPIView):
         with transaction.atomic(): 
             new_bet_list, total_bet_amount = serializer.save()
             # adjust the balance of the user 
-            queried_user = get_object_or_404(User, id=self.kwargs["user_id"])
-            queried_user.balance -= total_bet_amount
-            queried_user.save()
+            bet_owner = self.request.user
+            bet_owner.balance -= total_bet_amount
+            bet_owner.save()
         return new_bet_list
 
     # override create() method (which will be used by every other bet list views)
-    def create(self, request, **kwargs) -> Response: 
+    def create(self, request, *args, **kwargs) -> Response: 
         request_data = request.data
-        for item_data in request_data: item_data["user"] = self.kwargs["user_id"]
+        for item_data in request_data: item_data['user'] = request.user.pk # add user field to request data
 
         new_bet_list_serializer = self.get_serializer(data=request_data, many=True)
-        # automatically raise the exception
-        new_bet_list_serializer.is_valid(raise_exception=True) 
-        self.perform_create(new_bet_list_serializer) # create the bet
+        new_bet_list_serializer.is_valid(raise_exception=True) # automatically raise the exception
 
-        # query the list in which new moneyline bet has been added 
-        new_moneyline_bet_list = self.get_queryset()
-        new_list_serializer = self.get_serializer(new_moneyline_bet_list, many=True)
+        # the queryset of newly added user bets 
+        created_bet_list = self.perform_create(new_bet_list_serializer) 
         # return the response with the list 
-        return Response(new_list_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(self.get_serializer(created_bet_list, many=True).data, status=status.HTTP_201_CREATED)
     
 
 # views to list all of the user handicap bets (inheriting from UserBetList)
 class UserHandicapBetList(UserBetList): 
     serializer_class = UserHandicapBetSerializer
+    permission_classes = [IsAuthenticated]
 
     # the queryset (list of the user's handicap bets )
     def get_queryset(self): 
-        queried_user = get_object_or_404(User, id=self.kwargs["user_id"])
-        handicap_bet_list = UserHandicapBet.objects.filter(user=queried_user)
+        status = self.request.query_params.get('status')
+        if status is None: 
+            handicap_bet_list = UserHandicapBet.objects.filter(user=self.request.user)
+        else:
+            handicap_bet_list = UserHandicapBet.objects.filter(user=self.request.user, bet_info__status=status)
         return handicap_bet_list
     
 
 # views to list all of the user total goals bets (inheriting from UserBetList)
 class UserTotalGoalsBetList(UserBetList): 
     serializer_class = UserTotalObjectsBetSerializer
+    permission_classes = [IsAuthenticated]
 
     # the queryset (list of the user's total goals bets)
     def get_queryset(self):
-        queried_user = get_object_or_404(User, id=self.kwargs["user_id"])
-        total_goals_bet_list = UserTotalObjectsBet.objects.filter(user=queried_user)
-        return total_goals_bet_list
+        status = self.request.query_params.get("status")
+        if status is None:  
+            total_bet_list = UserTotalObjectsBet.objects.filter(user=self.request.user)
+        else: 
+            total_bet_list = UserTotalObjectsBet.objects.filter(user=self.request.user, bet_info__status=status)
+        return total_bet_list
     
 
 """
-generic views for 2 other types of bet details 
+Generic views for 2 other types of bet details 
 """
 # common views for handling the detail of view of all types of view
 class UserBetDetail(generics.RetrieveUpdateDestroyAPIView): 
@@ -173,30 +188,33 @@ class UserBetDetail(generics.RetrieveUpdateDestroyAPIView):
             updated_bet = serializer.save()
 
             # adjust the balance of the user 
-            queried_user = updated_bet.user 
+            bet_owner = updated_bet.user 
             bet_amount_difference = old_bet_amount - updated_bet.bet_amount
-            queried_user.balance += bet_amount_difference
-            queried_user.save()
+            bet_owner.balance += bet_amount_difference * 1.05 # extra fees 
+            bet_owner.save()
         return updated_bet
 
     # override the perform_destroy() method to give the amount the user bet back 
     def perform_destroy(self, instance): 
+        bet_validator.validate_delete(instance) # validate if the instance (bet) is elligible for withdrawing
         with transaction.atomic(): 
             # return the amount the user bet back to the user
-            this_user = instance.user 
-            this_user.balance += instance.bet_amount 
-            this_user.save()
+            bet_owner = instance.user 
+            bet_owner.balance += instance.bet_amount * 1.05 # extra fees 
+            bet_owner.save()
             # delete the monyeline bet from the database 
             instance.delete()
     
 
 # view to handle the detail of the handicap bet (inheritting from UserBetDetail)
 class UserHandicapBetDetail(UserBetDetail): 
+    permission_classes = [IsAuthenticated]
     queryset = UserHandicapBet.objects.all() # queryset from which the bet will be queried using pk
     serializer_class = UserHandicapBetSerializer # the serializer 
     
 
 # view to handle the detailf of the total goals bet (inheriting from UserBetDetail)
 class UserTotalGoalsBetDetail(UserBetDetail): 
+    permission_classes = [IsAuthenticated]
     queryset = UserTotalObjectsBet.objects.all()
-    serializer_class = UserHandicapBetSerializer
+    serializer_class = UserTotalObjectsBetSerializer
--- soccerapp/views/main_views.py
+++ soccerapp/views/main_views.py
@@ -3,39 +3,59 @@ ALL OF THE VIEWS IN HERE HAVE PERMISSION ALLOWANY, AND ARE ACCESSIBLE TO ANY END
 """
 
 from django.shortcuts import get_object_or_404
+from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.validators import ValidationError
 from soccerapp.models import (
-    Match, Team, TeamRanking,
+    User, Match, Team, TeamRanking,
     MoneylineBetInfo, HandicapBetInfo, TotalObjectsBetInfo,
 )
 from soccerapp.serializers import (
+    MyTokenObtainPairSerializer, RegisterSerializer,
     MatchSerializer, TeamSerializer, TeamRankingSerializer,
     MoneylineBetInfoSerializer, HandicapBetInfoSerizalizer, TotalObjectsBetInfoSerializer,
 )
 
 # mapping the value of request parameter to the value of the database 
-LEAGUES_NAME_MAP = {
-    "ucl": "Champions League", 
-    "epl": "Premiere League", 
-    "lal": "La Liga", 
-    "bun": "Bundesliga"
-}
-
+LEAGUES_NAME_MAP = { "ucl": "Champions League", "epl": "Premiere League", "lal": "La Liga", "bun": "Bundesliga"}
 # mapping the different format for time type 
 TIME_TYPE_MAP = {"half_time": "half time", "full_time": "full time"}
+
+# view to handle the user login 
+class Login(TokenObtainPairView): 
+    permission_classes = [AllowAny]
+    serializer_class = MyTokenObtainPairSerializer
+
+
+# view to handle the user register 
+class Register(generics.CreateAPIView): 
+    permission_classes = [AllowAny]
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
 
 # view to list all of the team according to the league 
 class TeamList(APIView): 
     permission_classes = [AllowAny]
 
-    # GET response 
-    def get(self, request, league: str, format=None) -> Response: 
+    # GET method
+    def get(self, request, format=None) -> Response: 
+        league_name = request.query_params.get("league")
+        if league_name is None: raise ValidationError({"error": "League not specified"})
+
         # get the list of teams of the league
-        team_list = Team.objects.filter(league=LEAGUES_NAME_MAP[league])
+        team_list = Team.objects.filter(league=LEAGUES_NAME_MAP[league_name])
         team_list_serizalizer = TeamSerializer(team_list, many=True)
         return Response(team_list_serizalizer.data)
+    
+
+class TeamDetail(generics.RetrieveAPIView): 
+    permission_classes = [AllowAny]
+    queryset = Team.objects.all() 
+    serializer_class = TeamSerializer
 
 
 # view to list all of the matches according to the league
@@ -43,41 +63,53 @@ class MatchList(APIView):
     permission_classes = [AllowAny]
 
     # GET method 
-    def get(self, request, league: str, status: str) -> Response: 
-        status_map = {
-            "NF": "Not Finished", 
-            "FN": "Finished"
-        }
-        if league == "all": 
+    def get(self, request, format=None) -> Response: 
+        status_map = {"NF": "Not Finished", "FN": "Finished"}
+        league = request.query_params.get("league")
+        status = request.query_params.get("status")
+
+        # status = None is not allowed for this endpoint 
+        if status is None: raise ValidationError({"error": "Status is not defined"})
+        if league is None: 
             match_list = Match.objects.filter(status=status_map[status])
         else: 
-            match_list = Match.objects.filter(
-                league=LEAGUES_NAME_MAP[league], status=status_map[status]
-            )
+            match_list = Match.objects.filter(league=LEAGUES_NAME_MAP[league], status=status_map[status])
         match_list_serializer = MatchSerializer(match_list, many=True)
         return Response(match_list_serializer.data)
-
     
+
+class MatchDetail(generics.RetrieveAPIView): 
+    permission_classes = [AllowAny]
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
+    lookup_field = "match_id"
+
+
 # view to handle the rankings of the league
 class Standings(APIView): 
     permission_classes = [AllowAny]
 
     # GET method
-    def get(self, request, league: str) -> Response: 
-        standings = TeamRanking.objects.filter(league=LEAGUES_NAME_MAP[league])
+    def get(self, request) -> Response: 
+        league_name = request.query_params.get("league")
+        if league_name is None: raise ValidationError({"error": "League not specified"})
+
+        standings = TeamRanking.objects.filter(league=LEAGUES_NAME_MAP[league_name])
         standings_serializer = TeamRankingSerializer(standings, many=True)
         return Response(standings_serializer.data)
      
-    
+
 """
 VIEWS FOR BET INFO
 """
-
 # view to list all of the moneyline bet info of the match with given match id
 class MoneylineInfoList(APIView): 
     permission_classes = [AllowAny]
 
-    def get(self, request, match_id: int, bet_obj: str, format=None) -> Response: 
+    def get(self, request, match_id: int, format=None) -> Response: 
+        bet_obj = request.query_params.get("bet_obj")
+        if bet_obj is None: raise ValidationError({"error": "Bet obj is not defined"})
+        
         # list of moneyline bets 
         queried_match = get_object_or_404(Match, match_id=match_id)
         moneyline_info_list = MoneylineBetInfo.objects.filter(match=queried_match, bet_object=bet_obj)
@@ -96,36 +128,39 @@ class MoneylineInfoList(APIView):
 # view to list all of the handicap bet info of the match 
 class HandicapInfoList(APIView):
     permission_classes = [AllowAny]
-
-    # group the handicap bet info that correspond to each other together
-    # the form [[Home1 -cover, Away1 cover], [Home2 -cover, Away2 cover], ...]
+    
+    """
+    group the handicap bet info that correspond to each other together
+    the form [[home1 -cover, away1 cover], [home2 -cover, away2 cover], ...]
+    """
     def group_handicap_info(self, handicap_info_list: list, home_team: str) -> list: 
-        # the dictionary mapping the cover to the correponding list of handicap info
-        cover_to_handicap_info_list = {}
+        cover_to_handicap_info = {} # the dictionary mapping the cover to the list of handicap info
         for handicap_info in handicap_info_list: 
             # the handicap cover of this item data 
+            cover = -float(handicap_info["handicap_cover"])
             if handicap_info["bet_team"] == home_team: 
-                this_cover = float(handicap_info["handicap_cover"])
-            else: 
-                this_cover = -float(handicap_info["handicap_cover"])
+                cover = float(handicap_info["handicap_cover"])
 
             # if this handicap cover isn't already in the list, add them 
-            if this_cover not in cover_to_handicap_info_list: 
-                cover_to_handicap_info_list[this_cover] = {"home": None, "away": None}
+            if cover not in cover_to_handicap_info: 
+                cover_to_handicap_info[cover] = {"home": None, "away": None}
 
             # put the handicap bet info into the right place of the list 
             if handicap_info["bet_team"] == home_team: 
-                cover_to_handicap_info_list[this_cover]["home"] = handicap_info
+                cover_to_handicap_info[cover]["home"] = handicap_info
             else: 
-                cover_to_handicap_info_list[this_cover]["away"] = handicap_info
+                cover_to_handicap_info[cover]["away"] = handicap_info
 
         # conver the dictionary to the list 
-        grouped_handicap_info_list = [grouped_info for grouped_info in list(cover_to_handicap_info_list.values())]
+        grouped_handicap_info_list = [grouped_info for grouped_info in list(cover_to_handicap_info.values())]
         # return the rearranged list of data 
         return grouped_handicap_info_list
     
     # GET method of the request 
-    def get(self, request, match_id: int, bet_obj: str, format=None) -> Response: 
+    def get(self, request, match_id: int, format=None) -> Response: 
+        bet_obj = request.query_params.get("bet_obj")
+        if bet_obj is None: raise ValidationError({"error": "Bet obj is not defined"})
+
         # the list of handicap bets 
         queried_match = get_object_or_404(Match, match_id=match_id)
         handicap_info_list = HandicapBetInfo.objects.filter(match=queried_match, bet_object=bet_obj)
@@ -136,53 +171,54 @@ class HandicapInfoList(APIView):
             # list and serialize handicap bet info for this time type
             type_handicap_list = handicap_info_list.filter(time_type=TIME_TYPE_MAP[time_type])
             type_handicap_data = HandicapBetInfoSerizalizer(type_handicap_list, many=True).data
-            response_data[time_type] = self.group_handicap_info(type_handicap_data, queried_match.home_team)
-            # response_data[time_type] = type_handicap_data # for testing 
-        # return the response data
+            response_data[time_type] = self.group_handicap_info(type_handicap_data, queried_match.home_team) 
         return Response(response_data)
 
 
 # views to list all of the total goals bet info of the match 
 class TotalObjectsInfoList(APIView):
     permission_classes = [AllowAny]
-
-    # group the the total goals bet infos that have the same number of target goals 
-    # the form [[Under num_goals1, Over num_goals1], [Under num_goals2, Over num_goals2], ...]
-    def group_total_objects_info(self, total_objects_info_list: list) -> list: 
-        # dict mapping the target number of goals available to corresponding list of bets 
-        num_to_objects_info_list = {}
-        for total_objects_info in total_objects_info_list: 
+    
+    """
+    group the the total goals bet infos that have the same number of target goals 
+    the form [[under num_goals1, over num_goals1], [under num_goals2, over num_goals2], ...]
+    """
+    def group_total_objects_info(self, total_objs_info_list: list) -> list: 
+        num_to_total_info = {}  # dict mapping the target number of objects to list of total bets 
+        for total_objects_info in total_objs_info_list:
             # the target number of goals 
             target_num_objects = total_objects_info["target_num_objects"]
 
             # if this number of goals isn't already in dict, add them
-            if target_num_objects not in num_to_objects_info_list: 
-                num_to_objects_info_list[target_num_objects] = {"under": None, "over": None}
+            if target_num_objects not in num_to_total_info: 
+                num_to_total_info[target_num_objects] = {"under": None, "over": None}
 
             # put the total-goals bet info into the right place of the list 
             if total_objects_info["under_or_over"] == "Under": 
-                num_to_objects_info_list[target_num_objects]["under"] = total_objects_info
+                num_to_total_info[target_num_objects]["under"] = total_objects_info
             else: 
-                num_to_objects_info_list[target_num_objects]["over"] = total_objects_info
+                num_to_total_info[target_num_objects]["over"] = total_objects_info
                 
         # convert dictionary into the list of grouped bet info
-        grouped_objs_info_list = [grouped_info for grouped_info in list(num_to_objects_info_list.values())]
-        return grouped_objs_info_list
+        grouped_total_info_list = [grouped_info for grouped_info in list(num_to_total_info.values())]
+        return grouped_total_info_list
     
     # GET method of the request 
-    def get(self, request, match_id: int, bet_obj: str, format=None) -> Response: 
+    def get(self, request, match_id: int, format=None) -> Response: 
+        bet_obj = request.query_params.get("bet_obj")
+        if bet_obj is None: raise ValidationError({"error": "Bet obj is not defined"})
+
         # get the list of total goals bet infos
         queried_match = get_object_or_404(Match, match_id=match_id)
-        goals_info_list = TotalObjectsBetInfo.objects.filter(match=queried_match, bet_object=bet_obj)
+        objs_info_list = TotalObjectsBetInfo.objects.filter(match=queried_match, bet_object=bet_obj)
         
         # the response data
         response_data = {"half_time": None, "full_time": None}
         for time_type in list(response_data.keys()): 
             # list of total goals bet info for this time type
-            type_goals_list = goals_info_list.filter(time_type=TIME_TYPE_MAP[time_type])
+            type_objs_list =   objs_info_list.filter(time_type=TIME_TYPE_MAP[time_type])
             # serialized data of the list 
-            type_goals_data = TotalObjectsBetInfoSerializer(type_goals_list, many=True).data
-            response_data[time_type] = self.group_total_objects_info(type_goals_data)
-            # response_data[time_type] = type_total_goals_data # for testing 
+            type_objs_data = TotalObjectsBetInfoSerializer(type_objs_list, many=True).data
+            response_data[time_type] = self.group_total_objects_info(type_objs_data)
         return Response(response_data)
     
