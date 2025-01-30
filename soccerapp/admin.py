6d283aacfd39f95a60f54df95fb8dc400205d8fa
mikehquan19 <hoangphucquan19@gmail.com>
2025-01-30 02:24:41 -0600

    Add Comment models to admin page

--- soccerapp/admin.py
+++ soccerapp/admin.py
@@ -3,6 +3,7 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.User)
+admin.site.register(models.Comment)
 admin.site.register(models.Team)
 admin.site.register(models.TeamRanking)
 admin.site.register(models.Match)
