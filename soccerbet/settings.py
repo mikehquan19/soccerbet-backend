05225e99f7aaabed6080ae40dd6a69eb3f805603
mikehquan19 <hoangphucquan19@gmail.com>
2025-11-11 10:57:38 -0600

    Fix deployment

--- soccerbet/settings.py
+++ soccerbet/settings.py
@@ -169,6 +169,7 @@ CELERY_TASK_TRACK_STARTED = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
